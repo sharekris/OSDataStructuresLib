5604f920f3dbb300ae1a48c41dac7f47c5e3aaff
Kris S <admin@kris-mpb.gateway.2wire.net>
2014-01-22 18:33:23 -0800

    Test checkin

---
+++ OSDataStructuresLib/Defines.h
@@ -0,0 +1,18 @@
+//
+//  Defines.h
+//  OSDataStructuresLib
+//
+//  Created by Kris Subramanian on 1/22/14.
+//
+//
+
+#ifndef OSDataStructuresLib_Defines_h
+#define OSDataStructuresLib_Defines_h
+
+typedef enum {
+    Directed = 0,
+    UnDirected = 1
+} GraphType;
+
+
+#endif
